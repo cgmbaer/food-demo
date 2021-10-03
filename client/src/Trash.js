f401a787ecfb210d88ad5ddc80613110a2594eeb
Stallione <cgmbaer@gmail.com>
2021-10-03 15:42:58 +0200

    .

--- client/src/Trash.js
+++ client/src/Trash.js
@@ -1,4 +1,7 @@
 import './App.css';
+import ImageIcon from '@mui/icons-material/Image';
+import WorkIcon from '@mui/icons-material/Work';
+import BeachAccessIcon from '@mui/icons-material/BeachAccess';
 
 export default function Trash() {
 
